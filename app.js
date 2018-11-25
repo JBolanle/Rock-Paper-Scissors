a3f0c3a4a24c4851f4d09dd8b333db9428d6a41a
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2018-11-25 02:23:28 -0500

    updating score and messages

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
@@ -18,6 +18,28 @@ function getCompChoice() {
     return choices[randomNumber];
 }
 
+function convertToWords(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    if (letter === "s") return "Scissors";
+}
+
+function win(userChoice, compChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = convertToWords(userChoice) + " beats " + convertToWords(compChoice) + ". You win!";
+    console.log(userChoice);
+    console.log(compScore);
+}
+
+function lose(){
+}
+
+function draw(){
+    console.log("Draw");
+}
+
 //game logic
 function game(userChoice) {
     const compChoice = getCompChoice();
@@ -25,19 +47,19 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-            console.log("User wins!");
+            win(userChoice, compChoice);
             break;
 
         case "rp":
         case "ps":
         case "sr":
-            console.log("User loses!");
+            lose(userChoice, compChoice);
             break;
 
         case "rr":
         case "pp":
         case "ss":
-            console.log("It's a tie!");
+            draw();
             break;
     }
 }
