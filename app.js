cba1df893f299c9fdd622ae8454648baceee5ee2
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2018-11-25 01:55:48 -0500

    function to select random choice for computer

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getCompChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+};
+
 function game(userChoice) {
     console.log("Stuff " + userChoice);
-}
+};
 
 function main() {
     //Event listeners
